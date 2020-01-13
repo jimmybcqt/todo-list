d87db7024f2d1c49c1afe5e91fb86e55abccf0ba
Jimmy Bocquet <jimmy.bocquet@accenture.com>
2020-01-13 14:01:24 +0100

    Clean useless thing and init project

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="Header">
+    <h1 class="Header-title">
+      Crazy todolist made with style (and some
+      <img src="@/assets/logo.png" height="20px" width="20px"/>)
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header.vue',
+};
+</script>
+
+<style scoped>
+
+</style>
